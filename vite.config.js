ed694f9a002789126ab00b4e2392923262a3c4dc
keepgoing114 <ckmaker23@gmail.com>
2026-06-15 16:45:39 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  /sigyebu-/,
+ base: '/sigyebu-/',
   plugins: [
     react(),
     VitePWA({
